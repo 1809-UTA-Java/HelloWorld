297d9375703672e32c8d40655125fd7cae0762af
MehrabRahman <mehrab.rahman@revature.com>
2018-09-25 10:48:42 -0500

    Moved repo

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package demo;
+
+//Fully Qualified Classname = demo.HelloWorld
+public class HelloWorld {
+	public static void main(String args[]) {
+		// Double forward slash - comment 
+		// until end of line
+		//System.out.println(args.length);
+
+		/* Prints Hello and first arg entry 
+			still commented out
+		*/
+		System.out.println("Hello, " + args[0]);
+	}
+}
